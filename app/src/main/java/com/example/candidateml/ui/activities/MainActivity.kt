a45944d26cb5e02a6552cee0182cf9825a453b61
David Eusebio Cazares Perez <david.cazares@interfactura.com>
2021-12-05 10:39:47 -0600

    refactor(main): splash

--- app/src/main/java/com/example/candidateml/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/candidateml/ui/activities/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.candidateml.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.candidateml.R
 import com.example.candidateml.ui.fragments.MainFragment
 import dagger.hilt.android.AndroidEntryPoint
@@ -11,6 +12,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Handle the splash screen transition.
+        val splashScreen = installSplashScreen()
+
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
